721f6250c58de3acfbb40d381e33833fc07aa0aa
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:13:41 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+-- Framework: 'esx' or 'qb'
+Config.Framework = 'esx'
+
+-- Phone item name
+Config.PhoneItem = 'phone'
+
+-- Phone usable only in MLO?
+Config.MLOOnly = true
+
+-- Allowed MLO zones
+Config.MLOZones = {
+    {
+        coords = vector3(-656.12, -858.32, 24.5),
+        radius = 6.0
+    }
+}
+
+-- Keybind
+Config.OpenKey = 'P'
+
+-- Animations
+Config.PhoneAnim = {
+    dict = 'cellphone@',
+    anim = 'cellphone_text_read_base'
+}
