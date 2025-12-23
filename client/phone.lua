82cf227f4ac0dcb50cc798185efb0bf33efc42b4
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:15:44 +0530

    Create phone.lua

---
+++ client/phone.lua
@@ -0,0 +1,35 @@
+local phoneOpen = false
+
+RegisterKeyMapping('openphone', 'Open Phone', 'keyboard', Config.OpenKey)
+
+RegisterCommand('openphone', function()
+    if phoneOpen then return end
+
+    if Config.MLOOnly and not IsPlayerInMLO() then
+        TriggerEvent('chat:addMessage', {
+            args = {'Phone', 'You can only use phone inside phone shop'}
+        })
+        return
+    end
+
+    PlayPhoneAnim()
+    phoneOpen = true
+    SetNuiFocus(true, true)
+    SendNUIMessage({ action = 'open' })
+end)
+
+RegisterNUICallback('close', function()
+    phoneOpen = false
+    StopPhoneAnim()
+    SetNuiFocus(false, false)
+end)
+
+function IsPlayerInMLO()
+    local coords = GetEntityCoords(PlayerPedId())
+    for _, zone in pairs(Config.MLOZones) do
+        if #(coords - zone.coords) < zone.radius then
+            return true
+        end
+    end
+    return false
+end
