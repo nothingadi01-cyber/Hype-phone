d553ecb25fda04d4184c4ddd7c849cb9159467cc
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:29:52 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -50,3 +50,13 @@ server_scripts {
     'server/framework.lua',
     'server/main.lua'
 }
+RegisterNUICallback('sendMessage', function(data)
+    TriggerServerEvent('lb-phone:sendMessage', data)
+end)
+
+RegisterNetEvent('lb-phone:receiveMessage', function(data)
+    SendNUIMessage({
+        action = 'newMessage',
+        data = data
+    })
+end)
