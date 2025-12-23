4a69ff54d4668f5133baff877534c742d29211d2
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:04:52 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'LB Style MLO Phone'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/*'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
