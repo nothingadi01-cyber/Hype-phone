82b8fae7a8007b357eb9b17a0bf266cea7b03bba
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:39:11 +0530

    Create camera.lua

---
+++ client/camera.lua
@@ -0,0 +1,14 @@
+RegisterNUICallback('takePhoto', function()
+    exports['screenshot-basic']:requestScreenshotUpload(
+        'http://localhost/upload', 'files[]',
+        function(data)
+            local image = json.decode(data).files[1].url
+            TriggerServerEvent('lb-phone:savePhoto', image)
+            SendNUIMessage({ action = 'photoSaved', image = image })
+        end
+    )
+end)
+
+RegisterNUICallback('loadGallery', function(data, cb)
+    TriggerServerEvent('lb-phone:getPhotos', cb)
+end)
