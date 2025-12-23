4b3a01aecb28ca4437faf77ea585281099324ac8
nothingadi01-cyber <nothingadi01@gmail.com>
2025-12-24 00:07:32 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -6,3 +6,9 @@ end)
 RegisterNUICallback("close", function()
     SetNuiFocus(false, false)
 end)
+
+local allowedCoords = vector3(-656.12, -858.32, 24.5)
+
+if #(GetEntityCoords(PlayerPedId()) - allowedCoords) < 5.0 then
+   -- allow phone
+end
